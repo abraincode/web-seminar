a483cf33d057df24fd674fe8f2dc60b1810b8813
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-15 11:04:58 +0700

    Update store

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Participant;
 
 class DashboardController extends Controller
 {
@@ -37,9 +38,35 @@ class DashboardController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function storeParticipant(Request $request)
     {
-        //
+        // validasi data
+        $this->validate($request, array(
+                'fname'      => 'required',
+                'lname'      => 'required|max:255',
+                'email'      => 'required',
+                'cell'       => 'required|numeric',
+                'address'    => 'required',
+                'zip'        => 'required|numeric',
+                'city'       => 'required',
+                'session'    => 'required',
+            ));
+
+        $participant = new Participant;
+        $participant->first_name = $request->fname;
+        $participant->last_name = $request->lname;
+        $participant->email = $request->email;
+        $participant->phone = $request->cell;
+        $participant->address = $request->address;
+        $participant->zip_code = $request->zip;
+        $participant->city = $request->city;
+        $participant->session = $request->session;
+        $participant->save();
+
+        // flash messages
+        $request->session()->flash('status', 'Registration Success!');
+        // redirect ke halaman
+        return redirect()->route('dashboard.home.index');
     }
 
     /**
